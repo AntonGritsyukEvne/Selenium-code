0667989ba762c24aa1c3065516f206d08620cb6c
kekcuk5 <anton.gritsyuk@gmail.com>
2021-05-04 09:44:38 +0300

    update files

--- Test/Test/java.kt
+++
@@ -1,9 +0,0 @@
-internal object Main {
-    fun main(args: Array<String?>?) {
-        System.out.println("WE NEED TO")
-        System.out.println("\n")
-        System.out.println("LEARN JAVA")
-        System.out.println("\n")
-        System.out.println("AS QUICKLY AS POSSIBLE")
-    }
-}
